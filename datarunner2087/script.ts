ae7a1a6a12296f08a1ae1d0c6dc8af5788145c4d
josngu <100111404+josngu@users.noreply.github.com>
2022-11-11 01:32:26 -0500

    Added the ability to randomize file names

--- datarunner2087/script.ts
+++ datarunner2087/script.ts
@@ -8,15 +8,40 @@ var warningCount = 0;
 var endDay = false;
 var startDay = false;
 
+var protocolViolated = false;
 var penaltyReason = "";
 var penaltyAmount = 0;
+
+var fileName = "";
+
+const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
 const FIRST_NAME = ["James", "Sofia", "Ethan", "Emma", "Carter", "Scarlett", "Nathan", "Nora", "Muhammad", "Fatima", "Hamza", "Maria", "Antonio", "Sonia", "Mohammad", "Mia", "Nikola", "Alice", "Francisco", "Lucy", "Niko", "June", "Hao", "Astrid", "Omar", "Mary", "Daniel", "Valentina", "Agustin", "Olivia", "Jacob", "Abigail", "Alex", "Agnes", "Martin", "Emily", "Liam", "Grace", "Leonardo", "Isabelle", "Niko", "Valerie", "Charlie", "Charlotte", "Bob", "Anastasia", "Kazuya", "Mikazuki", "Sergei", "Yoko", "Ringo", "Julia"];
 
 const LAST_NAME = ["Huang", "Gutierrez", "Robinson", "Watson", "Khalid", "Hasan", "Harrison", "Ferguson", "Cunningham", "Hoffman", "Chan", "Schroeder", "Weiss", "Singh", "Zhou", "Abubakar", "Romero", "Contreras", "Nakamura", "Yamamoto", "Kobayashi", "Blanco", "Torres", "Lee", "Harris", "Wright", "Flores", "Cooper", "Sullivan", "Delgado", "Atkinson", "Baxter", "McDonald", "McDuff", "McAllister", "McMahon", "McKee", "McGregor", "Fukumoto", "Kubota", "Ichikawa", "Nagata", "Aguilar", "Rivera", "Richardson"];
 
-//Ministry of education, energy, employment, security, transportation, health, finance, agriculture, immigration, infrastructure, defense, tourism, global affairs, public services
-const MINISTRY = ["ED", "EN", "EM", "SE", "TR", "HE", "FI", "AG", "IM", "IN", "DE", "TO", "GL", "PU"];
+const FILE_TYPE = ["a video file", "a video", "a photo", "a photograph", "a picture", "an image", "an audio file", "a document"];
+const VIDEO_TYPE = ["mov", "mp4", "avi", "flv", "webm"];
+const IMAGE_TYPE = ["jpg", "png", "webp", "tiff", "raw"];
+const AUDIO_TYPE = ["mp3", "ogg", "flac", "wav", "m4a"];
+const DOCUMENT_TYPE = ["docx", "odt", "txt", "rtf", "pdf"];
+
+const RESPONSE = ["I sent", "I sent you", "This is", "It's", "It is", "I've sent you", "The file should be", "The file is", "I think I sent", "I uploaded", "I've uploaded"];
+
+//Ministry of education, energy, employment, transportation, health, agriculture, infrastructure, defense
+const MINISTRY = ["ED", "EN", "EM", "TR", "HE", "AG", "IN", "DE"];
+
+const ED_KEYWORDS = ["teacher", "teachers", "school", "education", "college", "building", "instructor", "student", "students", "research"];
+const EN_KEYWORDS = ["power plant", "power", "electricity", "voltage", "energy", "building", "utilities"];
+const EM_KEYWORDS = ["work", "workforce", "labour", "employee", "employment", "workplace", "job", "industry", "coworker", "colleague", "career", "occupation", "corporation", "company", "enterprise", "organization"];
+const TR_KEYWORDS = ["road", "airplane", "plane", "jet plane", "flying", "airport", "train", "trains", "car", "highway", "ship", "ships", "boat", "vehicle", "vehicles"];
+const HE_KEYWORDS = ["health", "hospital", "doctor", "nurse", "healthcare", "physician", "surgeon", "medic", "clinic", "ward", "emergency room", "surgery"];
+const AG_KEYWORDS = ["farm", "farms", "farmland", "farmer", "crops", "agriculture", "harvest", "cultivation", "plowing"];
+const IN_KEYWORDS = ["building", "buildings", "skyscraper", "skyscrapers", "construction", "architecture", "house", "houses", "structure"];
+const DE_KEYWORDS = ["military", "war", "weapons", "guns", "firearms", "army", "artillery", "infantry", "corps", "armaments", "troops", "squadron", "squad"];
+
+const AUDIO_KEYWORDS = ["bass", "upbeat", "drums", "classical", "piano", "guitar", "trumpet", "saxophone", "relaxing", "violin"];
+const DOCUMENT_KEYWORDS = ["finance", "financial", "document", "spreadsheet", "article", "record", "records", "accounts", "management", "archive", "data"];
 
 const MONTH = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
@@ -26,28 +51,153 @@ function openConnection(){
         clock();
         openConnection();
     } else {
+        $("#btn-open-connection").css({"pointer-events":"none", "animation":"none", "opacity":"40%"});
+
         let firstName = FIRST_NAME[Math.floor(Math.random() * FIRST_NAME.length)];
         let lastName = LAST_NAME[Math.floor(Math.random() * LAST_NAME.length)];
-        
-        Promise.resolve().then(() => delay(1000))
+        let fileType = FILE_TYPE[Math.floor(Math.random() * FILE_TYPE.length)];
+        let response = RESPONSE[Math.floor(Math.random() * RESPONSE.length)];
+
+        Promise.resolve().then(() => delay(500))
         .then(() => $("#transcript-container div").append(`<p id="text-login-status">${firstName} ${lastName} has logged in</p>`))
-        .then (() => delay(500))
-        .then (() =>  $(".dialog-box").css("display", "flex"))
-        .then (() => delay(1000))
-        .then (() => $(".dialog-box p").text("FILE RECEIVED"))
-        .then (() => $(".dialog-box").addClass("dialog-box-exit"))
-        .then (() => delay(500))
-        .then (() => $("#metadata-container").css("display", "flex"))
-        .then (() => $(".dialog-box").removeClass("dialog-box-exit").hide())
-        .then (() => delay(1000))
-        .then (() => $("#transcript-container div").append(`<p class="name-admin">SysAdmin</p>
-        <p class="transcript-admin">What is the purpose of this file?</p>`))
-        .then (() => delay(2000))
-        .then (() => $("#transcript-container div").append(`<p class="name-user">${firstName} ${lastName}</p>
-        <p class="transcript-user">This is a video file.</p>`));
+        .then(() => delay(1000))
+        .then(() =>  $(".dialog-box").css("display", "flex"))
+        .then(() => delay(1000))
+        .then(() => {
+            $(".dialog-box p").text("FILE RECEIVED");
+            $(".dialog-box").addClass("dialog-box-exit");
+        })
+        .then(() => delay(500))
+        .then(() => {
+            $("#metadata-container").css("display", "flex");
+            $(".dialog-box").removeClass("dialog-box-exit").hide();
+            $(".dialog-box p").text("RECEIVING FILE...");
+        })
+        .then(() => delay(1000))
+        .then(() => $("#transcript-container div").append(`<p class="name-admin">SysAdmin</p>
+        <p class="transcript-admin">What is the file that you have sent?</p>`))
+        .then(() => delay(2000))
+        .then(() => $("#transcript-container div").append(`<p class="name-user">${firstName} ${lastName}</p>
+        <p class="transcript-user">${response} ${fileType}.</p>`));
+
+        generateMetadata(fileType);
     }
 }
+function generateMetadata(fileType: String){
+    violateProtocol();
+    switch(fileType){
+        case "a video file":
+        case "a video":
+            generateFileName("video");
+            break;
+        case "a photo":
+        case "a photograph":
+        case "a picture":
+        case "an image":
+            generateFileName("image");
+            break;
+        case "an audio file":
+            generateFileName("audio");
+            break;
+        case "a document":
+            generateFileName("document");
+    }
+}
+function generateFileName(fileType: String){
+    let ministry = MINISTRY[Math.floor(Math.random() * MINISTRY.length)];
+    if (fileType === "video" || fileType === "image"){
+        switch(ministry){
+            case "ED":
+                let ED = ["School", "Class", "College", "University", "Classroom", "LectureHall"];
+                fileName = "MOED_" + ED[Math.floor(Math.random() * ED.length)] + "_";
+                break;
+            case "EN":
+                let EN = ["NuclearPowerPlant", "CoalPowerPlant", "GasPowerPlant", "Windmill", "Windmills", "SolarPanel", "SolarPanels", "Geothermal", "Hydroelectric"];
+                fileName = "MOEN_" + EN[Math.floor(Math.random() * EN.length)] + "_";
+                break;
+            case "EM":
+                let EM = ["Office", "OfficeHeadquarters", "Offices", "Business", "Factory", "Manufacturer", "RetailStore"];
+                fileName = "MOEM_" + EM[Math.floor(Math.random() * EM.length)] + "_";
+                break;
+            case "TR":
+                let TR = ["Transportation", "Transport", "Shipping", "Transit", "Freight"];
+                fileName = "MOTR_" + TR[Math.floor(Math.random() * TR.length)] + "_";
+                break;
+            case "HE":
+                let HE = ["HealthClinic", "HospitalRoom", "NursingHome", "PsychiatricWard", "Infirmary", "IsolationWard"];
+                fileName = "MOHE_" + HE[Math.floor(Math.random() * HE.length)] + "_";
+                break;
+            case "AG":
+                let AG = ["TractorOnFarm", "FarmerCultivating", "FarmerHarvesting", "FarmerPlanting", "CountrysideFarm", "RooftopFarm"];
+                fileName = "MOAG_" + AG[Math.floor(Math.random() * AG.length)] + "_";
+                break;
+            case "IN":
+                let IN = ["CitySkyline", "Downtown", "CityDistrict", "UrbanDistrict", "CityArchitecture", "TallSkyscrapers", "TallBuildings"];
+                fileName = "MOIN_" + IN[Math.floor(Math.random() * IN.length)] + "_";
+                break;
+            case "DE":
+                let DE = ["MilitaryBunker", "Bootcamp", "MilitaryTraining", "Soldiers", "Battalion", "UndergroundBunker", "UnitFormation"];
+                fileName = "MODE_" + DE[Math.floor(Math.random() * DE.length)] + "_";
+                break;
+        }
+    }
+    if (fileType === "audio"){
+        let x = ["Audio", "Music"];
+        fileName = "MO" + ministry + "_" + x[Math.floor(Math.random() * x.length)] + "_";
+    }
+    if (fileType === "document"){
+        let x = ["Form", "Letter", "Application", "DataSheet", "Chart", "Publication", "Guidelines", "Statement", "Census", "Statistics"];
+        fileName = "MO" + ministry + "_" + x[Math.floor(Math.random() * x.length)] + "_";
+    }
+    //All of the code above generates the first half of the file name
+    //Next, we need to generate the date
+    fileName = fileName + generateDateShort();
+    //Lastly, we need a file extension
+    switch(fileType){
+        case "video":
 
+        case "image":
+
+        case "audio":
+
+        case "document":
+
+    }
+}
+function generateDateShort(){
+    let year = Math.floor(Math.random() * 39 + 2047);
+    let month = Math.floor(Math.random() * 11 + 1);
+    let day = Math.floor(Math.random() * 27 + 1);
+    return addLeadingZero(year, month, day);
+}
+function generateDateLong(){
+    let year = Math.floor(Math.random() * 39 + 2047);
+    let month = MONTH[Math.floor(Math.random() * MONTH.length)];
+    let day = Math.floor(Math.random() * 27 + 1);
+    return addLeadingZero(year, month, day);
+}
+function addLeadingZero(year:number, month:number|String, day:number){
+    if (month < 10 && day < 10){
+        return year + "M0" + month + "D0" + day;
+    }
+    if (month < 10){
+        return year + "M0" + month + "D" + day;
+    }
+    if (day < 10){
+        return year + "M" + month + "D0" + day;
+    }
+    return year + "M" + month + "D" + day;
+}
+function violateProtocol(){
+    let x = Math.floor(Math.random() * 1);
+    switch(x){
+        case 0:
+            protocolViolated = false;
+            break;
+        case 1:
+            protocolViolated = true;
+    }
+}
 function dayStart(){
     switch(day){
         case 1:
