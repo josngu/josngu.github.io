b01d106e346de326f012c96f81c8016c61c3a270
Newyenium <100111404+josngu@users.noreply.github.com>
2023-02-20 18:21:25 -0500

    Design changes

--- sun-seeds/src/components/Homepage.tsx
+++ sun-seeds/src/components/Homepage.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import HomeHero from '../components/HomeHero';
-import TwoCompartmentBox from '../components/BenefitsSection';
+import BenefitsSection from '../components/BenefitsSection';
 import Button from '../components/Button';
 
 function Homepage() {
   return (
     <>
       <HomeHero />
-      <TwoCompartmentBox />
+      <BenefitsSection />
       <section className='cta-section'>
         <h2>See our development process!</h2>
         <Button url='#/development-process' text='Learn more' />
